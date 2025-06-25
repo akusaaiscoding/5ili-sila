18fdb83b93860883d423c46662e2fd32b60f4f16
better-eat-butter <liemantorojonathan@gmail.com>
2025-06-25 07:31:16 +0700

    Add files via upload

---
+++ Ending/control.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var Panel1 = $end1
+@onready var Panel2 = $end2
+@onready var Panel3 = $end3
+@onready var Panel4 = $end4
+@onready var Panel5 = $end5
+@onready var Panel6 = $end6
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Panel1.visible = true
+	Panel2.visible = false
+	Panel3.visible = false
+	Panel4.visible = false
+	Panel5.visible = false
+	Panel6.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Ending/end_5_but.gd
@@ -0,0 +1,17 @@
+extends Button
+
+var float_amplitude := 2.0
+var float_speed := 0.5
+var base_position := Vector2()
+
+func _ready():
+	base_position = position
+
+func _process(delta):
+	position.y = base_position.y + sin(Time.get_ticks_msec() / 1000.0 * float_speed * TAU) * float_amplitude
+
+@onready var target_panel = $"../../end6"
+
+func _on_pressed():
+	get_parent().visible = false  # Hides the panel the button is inside
+	target_panel.visible = true 
